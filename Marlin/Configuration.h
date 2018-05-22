3de081c3b59b572ce9039bcdd0d370d5050c35b3
you <you@example.com>
2018-05-22 12:14:19 -0400

    testing acceleration and jerk settings for stringing reduction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -538,7 +538,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 4800, 4800, 1000, 50 } // LRW Modifed for CR-10s
+#define DEFAULT_MAX_FEEDRATE          { 4800, 4800, 1000, 250 } // LRW Modifed for CR-10s
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -557,7 +557,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves  // LRW Modifed for CR-10s
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts  // LRW Modifed for CR-10s
+#define DEFAULT_RETRACT_ACCELERATION  10000    // E acceleration for retracts  // LRW Modifed for CR-10s
 #define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves  // LRW Modifed for CR-10s
 
 /**
@@ -570,8 +570,8 @@
  */
 #define DEFAULT_XJERK                 10.0
 #define DEFAULT_YJERK                 10.0
-#define DEFAULT_ZJERK                  0.3
-#define DEFAULT_EJERK                 10.0
+#define DEFAULT_ZJERK                  0.4
+#define DEFAULT_EJERK                 12.0
 
 //===========================================================================
 //============================= Z Probe Options =============================
